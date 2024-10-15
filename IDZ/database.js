774588f6de78268cff3a9595b9a6d7695bb66b36
Korend_DS <64439063+Korend27@users.noreply.github.com>
2024-10-16 02:38:11 +0300

    Update database.js

--- IDZ/database.js
+++ IDZ/database.js
@@ -2,78 +2,127 @@ const products = [
     {
         id: 1,
         name: "GeForce RTX 4090",
-        price: 2300,
+        price: 2800,
         image: "images/rtx4090.png",
         category: "videocards"  // Категория: Видеокарты
     },
     {
         id: 2,
         name: "GeForce RTX 4080",
-        price: 2000,
+        price: 1100,
         image: "images/rtx4080.png",
         category: "videocards"  // Категория: Видеокарты
     },
     {
         id: 3,
         name: "GeForce RTX 4070",
-        price: 1500,
+        price: 800,
         image: "images/rtx4070.png",
         category: "videocards"  // Категория: Видеокарты
     },
     {
         id: 4,
-        name: "Intel Core i9",
-        price: 500,
-        image: "images/corei9.png",
+        name: "i9 14900kf",
+        price: 690,
+        image: "images/i9_14900kf.png",
         category: "processors"  // Категория: Процессоры
     },
     {
         id: 5,
-        name: "AMD Ryzen 9",
-        price: 450,
-        image: "images/ryzen9.png",
+        name: "i9 13900k",
+        price: 570,
+        image: "images/i9_13900k.png",
         category: "processors"  // Категория: Процессоры
     },
     {
         id: 6,
-        name: "Corsair RM850x",
-        price: 150,
-        image: "images/rm850x.png",
-        category: "psu"  // Категория: Блоки питания
+        name: "i9 12900K",
+        price: 440,
+        image: "images/i9_12900k.png",
+        category: "processors"  // Категория: Процессоры
     },
     {
         id: 7,
-        name: "Cooler Master MWE 650",
-        price: 100,
-        image: "images/mwe650.png",
+        name: "Asus ROG Strix 1000G",
+        price: 220,
+        image: "images/asus_rog_strix_1000g.png",
         category: "psu"  // Категория: Блоки питания
     },
     {
         id: 8,
-        name: "Corsair Vengeance 16GB RAM",
-        price: 80,
-        image: "images/vengeance16gb.png",
-        category: "ram"  // Категория: Оперативная память
+        name: "MSI MPG 750W",
+        price: 120,
+        image: "images/msi_mpg_750w.png",
+        category: "psu"  // Категория: Блоки питания
     },
     {
         id: 9,
-        name: "Kingston HyperX Fury 32GB RAM",
-        price: 150,
-        image: "images/hyperxfury32gb.png",
-        category: "ram"  // Категория: Оперативная память
+        name: "MSI MAG 550W",
+        price: 55,
+        image: "images/msi_mag_550w.png",
+        category: "psu"  // Категория: Блоки питания
     },
     {
         id: 10,
-        name: "NZXT H510",
-        price: 120,
-        image: "images/nzxt-h510.png",
-        category: "cases"  // Категория: Корпуса
+        name: "Kingston DDR5 64GB (2x32GB) 6000Mhz",
+        price: 250,
+        image: "images/kingston_ddr5_64gb_2x32gb_6000mhz.png",
+        category: "ram"  // Категория: Оперативная память
     },
     {
         id: 11,
-        name: "Fractal Design Meshify C",
-        price: 140,
-        image: "images/meshifyc.png",
+        name: "Kingston DDR5 32GB (2x16GB) 6400Mhz",
+        price: 150,
+        image: "images/kingston_ddr5_32gb_2x16gb_6400mhz.png",
+        category: "ram"  // Категория: Оперативная память
+    },
+    {
+        id: 12,
+        name: "Kingston DDR5 16GB (2x8GB) 6000MHz",
+        price: 80,
+        image: "images/kingston_ddr5_16gb_2x8gb_6000mhz.png",
+        category: "ram"  // Категория: Оперативная память
+    },
+    {
+        id: 13,
+        name: "JONSBO MOD-3 mini White",
+        price: 190,
+        image: "images/JONSBO_MOD_3_mini_White.png",
         category: "cases"  // Категория: Корпуса
+    },
+    {
+        id: 14,
+        name: "NZXT H6 Flow All White",
+        price: 140,
+        image: "images/NZXT_H6_Flow_All_White.png",
+        category: "cases"  // Категория: Корпуса
+    },
+    {
+        id: 15,
+        name: "MSI MAG Forge M100R Black",
+        price: 50,
+        image: "images/msi_mag_forge_m100r_black.png",
+        category: "cases"  // Категория: Корпуса
+    },
+    {
+        id: 16,
+        name: "Kingston FURY Renegade 3D NAND TLC 1TB",
+        price: 100,
+        image: "images/kingston_fury_renegade_3d_nand_tlc_1tb.png",
+        category: "memory"  // Категория: Накопители
+    },
+    {
+        id: 17,
+        name: "Kingston SSDNow A400 960GB",
+        price: 70,
+        image: "images/Kingston_SSDNow_A400_960GB.png",
+        category: "memory"  // Категория: Накопители
+    },
+    {
+        id: 18,
+        name: "Kingston FURY Renegade 500GB",
+        price: 75,
+        image: "images/Kingston_FURY_Renegade_500GB.png",
+        category: "memory"  // Категория: Накопители
     }
 ];
